97e3ea5439da62f92e47fc44b26d21bf946039f8
wings <774886347@qq.com>
2020-07-06 08:10:22 +0800

    最小栈的实现

---
+++ src/day0706/MinStack.java
@@ -0,0 +1,47 @@
+package day0706;
+
+import java.util.Stack;
+
+/**
+ * 最小栈的实现
+ */
+public class MinStack {
+    private Stack<Integer> mainStack = new Stack<Integer>();
+    private Stack<Integer> minStack = new Stack<>();
+
+    public void push(int element) {
+        mainStack.push(element);
+        if (minStack.empty() || element <= minStack.peek()) {
+            minStack.push(element);
+        }
+    }
+
+    public Integer pop() {
+        if (mainStack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        return mainStack.pop();
+    }
+
+    public int getMin() throws Exception {
+        if (mainStack.empty()) {
+            throw new Exception("stack is empty!");
+        }
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) throws Exception {
+        MinStack stack = new MinStack();
+        stack.push(4);
+        stack.push(9);
+        stack.push(7);
+        stack.push(3);
+        stack.push(8);
+        stack.push(5);
+        System.out.println(stack.getMin());
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        System.out.println(stack.getMin());
+    }
+}
